35a1e5fa5bba6a5aa73c6eab71cbe83b1eadcd69
elmeradrianv <elmeradrianv@outlook.com>
2023-04-19 12:39:23 -0600

    Making the generator for the tables of each phases

--- TwoPhases.py
+++ TwoPhases.py
@@ -1,11 +1,58 @@
+#%%
 import numpy as np
 from SimplexAlgorithm import simplex
-
+#%%
 def generate_simplex_table_first_phase(matrix_A, vector_b):
-    raise NotImplementedError
-def from_first_phase_generate_simplex_table_second_phase():
-    raise NotImplementedError
-def get_solutions(final_table_2p):
+    n,m =  np.shape(matrix_A)
+    table_0 = np.zeros((n+1,m+n+1))
+    # Filling the matrix A's part of the table
+    for i in range(n):
+        for j in range(m):
+           table_0[i,j] =  matrix_A[i,j]
+    # Filling the identity part of the table
+    for i in range(n):
+        table_0[i,m+i] = 1
+    # Filling the vector b's part of the table
+    for i in range(n):
+        table_0[i,-1] =  vector_b[i]
+    # Filling the vector cost part of the table
+    for i in range(n):
+        table_0[-1,m+i] = 1
+    return table_0
+#%%
+## Pruebas
+A = np.matrix([[1,2],[1,2]])
+b = np.array([7,8])
+
+generate_simplex_table_first_phase(A,b)
+#%%
+def from_first_phase_generate_simplex_table_second_phase(final_table_1p, vector_c):
+    n1P,m1P =  np.shape(final_table_1p)
+    n, m = n1P - 1, m1P-n1P
+    table_0 = np.zeros((n+1,m+1))
+    # Filling the matrix A's part of the table
+    for i in range(n):
+        for j in range(m):
+           table_0[i,j] =  final_table_1p[i,j]
+    print(table_0)
+    # Filling the vector b's part of the table
+    for i in range(n):
+        table_0[i,-1] =  final_table_1p[i,-1] 
+    print(table_0)
+    # Filling the vector cost part of the table
+    for i in range(m):
+        table_0[-1,i] = vector_c[i]
+    print(table_0)
+    return table_0
+#%% Pruebas
+A =np.matrix([[1.,0.,1.,2.5,0.,7.],
+               [0.,1.,0.,1.,1.5,8.],
+               [0.,0., 0,5., 5. ,0.]])
+c = [1,2,3]
+from_first_phase_generate_simplex_table_second_phase(A,c)
+
+#%%
+def get_solutions_simplex(final_table):
     raise NotImplementedError
 
 def TwoPhases(matrix_A, vector_b, vector_c):
@@ -16,7 +63,8 @@ def TwoPhases(matrix_A, vector_b, vector_c):
     # Second phase
     if final_table_1p[-1][-1] != 0:
         raise Exception
-    table_0_2p = from_first_phase_generate_simplex_table_second_phase(final_table_1p)
+    table_0_2p = from_first_phase_generate_simplex_table_second_phase(final_table_1p,vector_c)
     final_table_2p = simplex(table_0_2p)
-    vector_sol, min_func_obj=get_solutions(final_table_2p)
+    vector_sol, min_func_obj = get_solutions_simplex(final_table_2p)
     return vector_sol, min_func_obj
+# %%
