b11a03aef7092093b0b697960e506db9b923021b
dariosotelo <cesardariosotelo@gmail.com>
2023-04-23 17:05:41 -0600

    added B and C trials

--- SimplexAlgorithm.py
+++ SimplexAlgorithm.py
@@ -165,18 +165,4 @@ def identityInsideMatrix(table):
     return table
 
 
-#Test
-A = np.matrix([
-    [0.,5.,50.,1.,1.,0.,10.],
-    [1.,-15.,2.,0.,0.,7.,2.],
-    [0.,1.,1.,0.,1.,1.,6.],
-    [0.,-10.,-2.,0.,1.,0.,-6.],
-])
-
-print(A)
-A=identityInsideMatrix(A)
-
-print(A)
-
-
 # %%
