651cf4f3e6733a106300552c95cde3eec8318ae0
dariosotelo <cesardariosotelo@gmail.com>
2023-04-23 16:51:30 -0600

    main tests

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ from SimplexAlgorithm import *
 from TwoPhases import *
 
 if __name__=='__main__':
-    from SimplexAlgorithm import *
-    from TwoPhases import *
+    A = np.matrix([
+        [0.,5.,50.,1.,1.,0.,10.],
+        [1.,-15.,2.,0.,0.,7.,2.],
+        [0.,1.,1.,0.,1.,1.,6.],
+        [0.,-10.,-2.,0.,1.,0.,-6.],
+    ])
+    print(A)
+    A=identityInsideMatrix(A)
+    print(A)
